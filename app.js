5bb541807cbdd1e24dea60dfb761cbf3ccee5c4e
usmyc <hllsmtyc@gmail.com>
2023-04-18 12:49:43 +0200

    ss

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { json, urlencoded } from "express";
 import { peopleRouter } from "./Routes/people.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("app"));
+app.use(express.static(path.join(__dirname, "app")));
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
